17de8632167826c750d509b66a021e5af5757ac8
The Wizard <wizardofgunmonkeys@gmail.com>
2016-05-20 13:50:55 -0500

    EOD

--- FDC Tools/FDC_Tools/App.cs
+++ FDC Tools/FDC_Tools/App.cs
@@ -12,19 +12,20 @@ namespace FDC_Tools
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new StatusMain;
+            //{
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
+
         }
 
         protected override void OnStart()
