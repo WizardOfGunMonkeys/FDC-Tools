783dc6b99dd5c0a15b1f6076f85c2046b59458cc
The Wizard <wizardofgunmonkeys@gmail.com>
2016-05-20 13:42:14 -0500

    Add listview

--- FDC Tools/FDC_Tools/StatusMain.xaml.cs
+++ FDC Tools/FDC_Tools/StatusMain.xaml.cs
@@ -8,11 +8,43 @@ using Xamarin.Forms;
 
 namespace FDC_Tools
 {
+    static PageTypeGroup()
+    {
+        List<PageTypeGroup> Groups = new List<PageTypeGroup> {
+            new PageTypeGroup ("Alfa", "A"){
+                new PageModel("Amelia", "Cedar", new switchCellPage(),""),
+                new PageModel("Alfie", "Spruce", new switchCellPage(), "grapefruit.jpg"),
+                new PageModel("Ava", "Pine", new switchCellPage(), "grapefruit.jpg"),
+                new PageModel("Archie", "Maple", new switchCellPage(), "grapefruit.jpg")
+            },
+            new PageTypeGroup ("Bravo", "B"){
+                new PageModel("Brooke", "Lumia", new switchCellPage(),""),
+                new PageModel("Bobby", "Xperia", new switchCellPage(), "grapefruit.jpg"),
+                new PageModel("Bella", "Desire", new switchCellPage(), "grapefruit.jpg"),
+                new PageModel("Ben", "Chocolate", new switchCellPage(), "grapefruit.jpg")
+            }
+        }
+        All = Groups; //set the publicly accessible list
+    }
     public partial class StatusMain : ContentPage
     {
         public StatusMain()
         {
             InitializeComponent();
         }
+
+    }
+    public class PageTypeGroup : List<PageModel>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; } //will be used for jump lists
+        public string Subtitle { get; set; }
+        private PageTypeGroup(string title, string shortName)
+        {
+            Title = title;
+            ShortName = shortName;
+        }
+
+        public static IList<PageTypeGroup> All { private set; get; }
     }
 }
